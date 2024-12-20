766549b21113f5a4d163b0de8e13011247c97786
JRHacka <jakerice1999@gmail.com>
2024-12-19 20:32:52 -0600

    Here is the script I was using for my loading testing

--- testingloading.sh
+++ testingloading.sh
@@ -4,10 +4,10 @@ echo "Running an nmap scan against 10.10.10.245"
 nmap -sT -sV -Pn -p- -A -T3 10.10.10.245 &
 pid=$!
 while kill -0 $pid 2>/dev/null; do
-    chars="/-\|"
+    chars="/—\|"
     i=0
     while kill -0 $pid 2>/dev/null; do
-        figlet "\r${chars:i++%4:1}"
-        sleep 0.3
+        printf "\r${chars:i++%5:1}"
+        sleep 0.1
     done
 done
