727a904692adaa68a9787f412d6aa670c0d3cfa3
JRHacka <jakerice1999@gmail.com>
2024-12-19 19:53:25 -0600

    Adding my scanner script.

--- scannerscript.sh
+++ scannerscript.sh
@@ -2,6 +2,8 @@
 #A script to automate my nmap scanning 
 filelocal="$HOME/TargetScans/"
 cur_date="$( date '+%F_%H:%M:%S' )"
+
+figlet "Scanner Script" | lolcat -a -s 100 | boxes -d parchment
 read -p "What directory name would you like to use for this data?" dirname
 read -p "What IP would you like to target?" ipaddy
 
@@ -12,12 +14,21 @@ check_directory() {
 
 		if [ -d $filelocal$dirname ]; then
 			echo "Running an nmap scan against $ipaddy"
-			nmap -sT -sV -Pn -p- -A -T3 $ipaddy -oN "$filelocal""$dirname"/"nmap_""$ipaddy""$cur_date"
+			nmap -sT -sV -Pn -p- -A -T3 $ipaddy -oN "$filelocal""$dirname"/"nmap_""$ipaddy""$cur_date" &
+			pid=$!
+			while kill -0 $pid 2>/dev/null; do
+			    chars="/-\|/-\|/-\|/-\|"
+			    i=0
+			    while kill -0 $pid 2>/dev/null; do
+			        printf "\r${chars:i++%4:1}"
+			        sleep 0.3
+			    done
+			done
 			echo "NMAP scan complete, and results are stored here:"
 			echo "$filelocal""$dirname"/"nmap_""$ipaddy""$cur_date"
-			wfuzz -c -z file,/usr/share/wfuzz/wordlist/general/common.txt --hc 404 http://"$ipaddy"/FUZZ > "$filelocal""$dirname"/"wfuzz_""$ipaddy""$cur_date"
+			wfuzz -c -z file,/usr/share/wfuzz/wordlist/general/common.txt --hc 404 http://"$ipaddy"/FUZZ > "$filelocal""$dirname"/"wfuzz_""$ipaddy""_""$cur_date"
 			echo "WFUZZ scan complete, and results are stored here:"
-			echo "$filelocal""$dirname"/"wfuzz""$ipaddy""$cur_date"
+			echo "$filelocal""$dirname"/"wfuzz_""$ipaddy""_""$cur_date"
 			break
 		else
 			echo "The directory did not exist, but has been created."
@@ -28,6 +39,3 @@ check_directory() {
 
 #Call our function
 check_directory
-
-# Add logic to run nslookup(potentially rename file something more useful is the goal),
-# dirbuster, and create a more specific directory
