11ac46fed138e7b2bf2be4604d56468d6d68c8e0
JRHacka <jakerice1999@gmail.com>
2024-12-20 17:49:39 -0600

    Trying to get script to add a whole directory. Added an elif to handle it

--- autogit.sh
+++ autogit.sh
@@ -30,18 +30,29 @@ check_filename() {
 		read -p "What do you want the commit comment to say? " commitcomment
 
 		# Check if the file exists an if so push it to github
-		if [ -f $filename ]; then
-			echo "Git adding $filename"
-			git add $filename
-			sleep .5s
+		if [ -f "$filename" ]; then
+			echo "Git adding ""$filename""
+			git add "$filename""
+			sleep 0.5s
 			echo "Done. Now adding the commit comment"
 			git commit -m "$commitcomment"
-			sleep .5s
+			sleep 0.5s
 			echo "Done. Now adding to the main branch. Please enter your usernmae and password below."
 			git branch -M main
 			git push -u origin main
 			break
 		# If the file doesn't exist try again
+		elif [ "$filename" == "." ]; then
+			echo "Git adding this directory"
+			git add "$filename"
+			sleep 0.5s
+			echo "Done. Now adding the commit comment"
+			git commit -m "$commitcomment"
+			sleep 0.5s
+			echo "Done. Now adding to the main branch. Please enter your usernmae and password below."
+			git branch -M main
+			git push -u origin main
+			break
 		else
 			echo "The file $filename does not exist. Please try again."
 			echo "Here are your options:"
